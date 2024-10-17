af67eff58b90b687f52b8e189f2fefeba230c4a5
webbalenciaproject <web_balelcia_project@mail.ru>
2024-10-17 08:43:00 +0300

    websoket  hosting

--- src/app/shared/services/web-socket.service.ts
+++ src/app/shared/services/web-socket.service.ts
@@ -4,5 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WebSocketService {
-  socket = new WebSocket('ws://localhost:3500');
+  // socket = new WebSocket('ws://localhost:3500');
+  socket = new WebSocket('wss://xn--80aneajyecjh1b5f.xn--p1ai_host/wss');
 }
